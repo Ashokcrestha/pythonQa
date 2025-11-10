d36b1d6b2c76e83422e0d1f900b94e744d299036
Ashokcrestha <ashokstha51@gmail.com>
2025-11-10 17:05:47 +0545

    first code for py

---
+++ try.py
@@ -0,0 +1,52 @@
+x=10
+print(x)
+print(type(x))
+
+PTE=3.14
+print(PTE)
+print(type(PTE))
+
+myname="ashok"
+print(myname)
+print(type(myname))
+
+#list
+
+fruits=["apple" , "pineapple", "orange"]
+fruits.append("litchi")
+print(fruits)
+print(type(fruits))
+
+
+#dict 
+
+student={
+    "name":"ram",
+    "age": 21
+}
+print(student)
+print(type(student))
+
+#tuple
+
+weeks=("sunday", "monday", "tuesday", "wednesday","thursday","friday","saturday")
+print(weeks)
+print(type(weeks))
+
+
+#take user input
+
+name = input("enter your name:")
+print("hello", name)
+
+age = input("enter your age:")
+print("hello your age is ", age)
+
+z= 3+4
+print(z)
+
+print(5==5)
+
+value=40
+value+=value
+print(value)
