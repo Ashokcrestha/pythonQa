6075a6fde02e310f3b8826740562a7eca0002454
Ashokcrestha <ashokstha51@gmail.com>
2025-11-17 16:03:06 +0545

    added oop main pillars

--- oop.py
+++ oop.py
@@ -59,7 +59,65 @@ lily=flower("lily","white",500)
 lily.flowercolor()
 lily.flowerprice()
 
+
+#Main pillars of OOP (encapuslation,abstarction,inhertiance, polymorphism)
+
+#encapuslation - keeping data(variable) & actions(methods) together in a unite class
+class Login:
+ def __init__(self, username, password):
+    self.__username = username   
+    self.__password = password   
+ def get_username(self):
+    return self.__username   
+ def set_password(self, new_pass):
+    self.__password = new_pass  
+
+user = Login("ram123", "pass123")
+print(user.get_username())
+user.set_password("newPass")
+
+
+#Abstarction - hiding complexity , showing only important detail
+class Laptop:
+    def turn_on(self):
+        print("Laptop is turning on...")
+    # inside: CPU starts, RAM loads, OS boots (hidden)
     
+l = Laptop()
+l.turn_on()
+
+
+#inhertiance - inherit from parent class to child class
+class User:
+    def login(self):
+        print("User logged in")
+
+class Admin(User):   # Admin inherits login()
+    def delete_user(self):
+        print("User deleted")
+
+a = Admin()
+a.login()      # inherited
+a.delete_user()
+
+
+
+
+#polymorphism - same function different behaviour
+class Chrome:
+    def run_test(self):
+        print("Running test on Chrome")
+
+class Firefox:
+    def run_test(self):
+        print("Running test on Firefox")
+
+for browser in (Chrome(), Firefox()):
+    browser.run_test()
+
+
+
+
 
 
         
